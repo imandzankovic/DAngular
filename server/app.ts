3838043ffcfc9fb8f20fd6b38fc1b8391ab7e3e4
imandzankovic <fruit.10@hotmail.com>
2019-11-18 14:20:40 +0100

    fixes

--- server/app.ts
+++ server/app.ts
@@ -6,12 +6,26 @@ import * as path from 'path';
 
 import setRoutes from './routes';
 
+
 const app = express();
+const bodyParser = require("body-parser");
+//var server = app.listen(3000);
 dotenv.config();
+//require the http module
+const http = require('http').Server(app)
+
+// require the socket.io module
+const io = require('socket.io');
+const socket = io(http);
+const port = process.env.PORT || 3000;
+
 app.set('port', (process.env.PORT || 3000));
+
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(express.json());
+//bodyparser middleware
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 let mongodbURI;
@@ -25,7 +39,7 @@ if (process.env.NODE_ENV === 'test') {
 mongoose.Promise = global.Promise;
 
 
-mongoose.connect(mongodbURI,{useNewUrlParser: true})
+mongoose.connect(mongodbURI, { useNewUrlParser: true })
   .then(db => {
     console.log('Connected to MongoDB');
 
@@ -35,10 +49,30 @@ mongoose.connect(mongodbURI,{useNewUrlParser: true})
       res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
-    if (!module.parent) {
-      app.listen(app.get('port'), () => console.log(`Angular Full Stack listening on port ${app.get('port')}`));
-    }
+    // if (!module.parent) {
+    //   app.listen(app.get('port'), () => console.log(`Angular Full Stack listening on port ${app.get('port')}`));
+    // }
   })
   .catch(err => console.error(err));
+
+
+  //setup event listener
+socket.on("connection", socket => {
+  console.log("user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", message => {
+    console.log("Message Received: " + message);
+    // io.emit("message", { type: "new-message", text: message });
+    socket.broadcast.emit("message", { type: "new-message", text: message });
+  });
+});
+
+http.listen(app.get('port'), () => {
+  console.log("Running on Port: " + app.get('port'));
+});
 
 export { app };
