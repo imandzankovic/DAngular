e0d6fd28d5b129ba3cabaf6a74132ea55d68512a
imandzankovic <fruit.10@hotmail.com>
2019-11-18 16:02:45 +0100

    fix

--- server/app.ts
+++ server/app.ts
@@ -2,31 +2,52 @@ import * as dotenv from 'dotenv';
 //import * as express from 'express';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
-import * as path from 'path';
+//import * as path from 'path';
 
 import setRoutes from './routes';
-var express = require('express'),
- app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 const bodyParser = require("body-parser");
+
+const app = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .use('/', express.static(path.join(__dirname, '../public')))
+  .use(express.json())
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(app);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+
+  socket.on("message", message => {
+  console.log("Message Received: " + message);
+  // io.emit("message", { type: "new-message", text: message });
+  io.broadcast.emit("message", { type: "new-message", text: message });
+});
+});
+
+
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
 //var server = app.listen(3000);
 dotenv.config();
 
-app.use('/', express.static(path.join(__dirname, '../public')));
-app.use(express.json());
-//bodyparser middleware
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
 
 let mongodbURI;
 if (process.env.NODE_ENV === 'test') {
   mongodbURI = process.env.MONGODB_TEST_URI;
 } else {
   mongodbURI = process.env.MONGODB_URI;
-  app.use(morgan('dev'));
+  express().use(morgan('dev'));
 }
 
 mongoose.Promise = global.Promise;
@@ -50,18 +71,18 @@ mongoose.connect(mongodbURI, { useNewUrlParser: true })
 
 
   //setup event listener
-io.sockets.on("connection", socket => {
-  console.log("user connected");
+// io.sockets.on("connection", socket => {
+//   console.log("user connected");
 
-  socket.on("disconnect", function() {
-    console.log("user disconnected");
-  });
+//   socket.on("disconnect", function() {
+//     console.log("user disconnected");
+//   });
 
-  socket.on("message", message => {
-    console.log("Message Received: " + message);
-    // io.emit("message", { type: "new-message", text: message });
-    socket.broadcast.emit("message", { type: "new-message", text: message });
-  });
-});
+//   socket.on("message", message => {
+//     console.log("Message Received: " + message);
+//     // io.emit("message", { type: "new-message", text: message });
+//     socket.broadcast.emit("message", { type: "new-message", text: message });
+//   });
+// });
 
 export { app };
